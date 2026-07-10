7ff4393221020d3ff761a5c670cb3e78a2a516ad
derey79 <devyreymond@gmail.com>
2026-07-10 20:09:27 +0700

    update form register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/features/auth/components/RegisterForm';
 
 export default function RegisterPage() {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center bg-neutral-50 px-4'>
+    <div className='min-h-screen w-full flex items-center justify-center px-4'>
       <RegisterForm />
     </div>
   );
