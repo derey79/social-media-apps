dcb84e10b8daed4953a5853d6c7993cd13e7fc2b
derey79 <devyreymond@gmail.com>
2026-07-10 20:08:51 +0700

    update form login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/features/auth/components/LoginForm';
 export default function LoginPage() {
   return (
     // Menggunakan tata letak Flexbox untuk memosisikan form tepat di tengah layar browser
-    <div className='min-h-screen w-full flex items-center justify-center bg-neutral-50 px-4'>
+    <div className='min-h-screen w-full flex items-center justify-center px-4'>
       <LoginForm />
     </div>
   );
