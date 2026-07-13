ab202ed8f38cca611f61747739a1f45bbf259e47
derey79 <devyreymond@gmail.com>
2026-07-13 21:09:20 +0700

    update config file

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    dangerouslyAllowSVG: true, // Mengizinkan aset SVG jika ada ke depannya
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', // 💡 Mengunci izin untuk seluruh aset gambar Cloudinary Anda
+      },
+    ],
+  },
 };
 
 export default nextConfig;
