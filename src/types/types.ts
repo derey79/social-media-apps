59538e23b146f984754ceecafc4280eb9de37006
derey79 <devyreymond@gmail.com>
2026-07-13 23:13:20 +0700

    update types for add declaration new interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,8 +9,59 @@ export interface UserProfile {
   avatarUrl?: string | null;
 }
 
+export interface UserStats {
+  posts: number;
+  followers: number;
+  following: number;
+  likes: number;
+}
+
 export interface AuthState {
   token: string | null;
   user: UserProfile | null;
+  stats: UserStats | null; // 💡 2. Daftarkan wadah penampung stats di sini
   isAuthenticated: boolean;
+}
+
+// post
+export interface PostAuthor {
+  id: number;
+  username: string;
+  name: string;
+  avatarUrl: string | null;
+}
+
+export interface PostItem {
+  id: number;
+  imageUrl: string | null;
+  caption: string;
+  createdAt: string;
+  author: PostAuthor;
+  likeCount: number;
+  commentCount: number;
+  likedByMe: boolean;
+}
+
+// follow unfollow
+interface FollowUserItem {
+  id: number;
+  username: string;
+  name: string;
+  avatarUrl: string | null;
+  isFollowedByMe: boolean;
+}
+
+export interface FollowApiResponse {
+  success: boolean;
+  message: string;
+  data: {
+    users: FollowUserItem[];
+  };
+}
+
+export interface FollowModalProps {
+  username: string;
+  type: 'followers' | 'following';
+  onClose: () => void;
+  isMyProfile?: boolean;
 }
