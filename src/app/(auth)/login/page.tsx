85d66a13d5b94bd69be42e11cb80b56a0e594ff0
derey79 <devyreymond@gmail.com>
2026-07-14 22:35:08 +0700

    update import types and background color

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
-// 💡 1. Pastikan jalur impor mengarah ke komponen LoginForm yang bebas tipe 'any' kemarin
 import LoginForm from '@/features/auth/components/LoginForm';
 
-// 💡 2. WAJIB menggunakan 'export default function' agar dikenali oleh Router Next.js
 export default function LoginPage() {
   return (
     // Menggunakan tata letak Flexbox untuk memosisikan form tepat di tengah layar browser
